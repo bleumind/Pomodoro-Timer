8cd6d80700e758d82f2a104e07d8e3350dc7d603
Kenneth Lee <84689781+bleumind@users.noreply.github.com>
2021-08-15 01:55:10 -0700

    Update pomo.js

--- pomo.js
+++ pomo.js
@@ -2,41 +2,120 @@
 // Elements
 const play = document.querySelector(".resume");
 const pause = document.querySelector(".pause");
-const restart = document.querySelector(".restart");
+const restart = document.querySelector(".restart-btn");
 const timer = document.querySelector("#timer");
+const header = document.querySelector(".heading");
+const breakTimer = document.querySelector("#break-timer");
+let extraStudyTimer = document.querySelector("#extra-study-timer");
+const fadeEffect = document.querySelector(".restart-fade");
+const totalTime = document.querySelector("#total-time");
 
-// Timer
-const startingMinutes = 25;
-let time = startingMinutes * 60;
+// Progress Timer
+let startingMinutes = 1;
+let breakMinutes = 1;
+let time = (t) => t * 60;
+let mainTime = time(startingMinutes);
+let breakTime = time(breakMinutes);
+
 let isRunning = false;
-let timerActive;
+let xsIsRunning = false;
+let breakIsRunning = false;
+let breakIsRunningToggle = 0;
 
+let timerActive;
+let breakTimerActive;
+
+let xsTimerActive;
+let xsMinuteTime = 0;
+let xsTime = 0;
+
+// Updating Every Timer Count
 function updateTimer() {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
+  let minutes = Math.floor(mainTime / 60);
+  minutes = minutes >= 10 ? minutes : "0" + minutes;
+
+  let seconds = mainTime % 60;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  timer.innerHTML = `${minutes} : ${seconds}`;
-  time--;
-  if (time === 0) {
+  timer.innerHTML = `${minutes}:${seconds}`;
+  mainTime--;
+
+  if (minutes === "00" && seconds === "00") {
     clearInterval(timerActive);
+    isRunning = false;
+    xsTimerActive = setInterval(updateXStimer, 1000);
+    xsIsRunning = true;
+    extraStudyTimer.style.display = "block";
+    mainTime = time(startingMinutes);
+  }
+  if (!isRunning) {
+    restart.style.display = "none";
   }
 }
 
+function updateXStimer() {
+  if (xsTime > 59) {
+    xsTime = 0;
+    xsMinuteTime++;
+  }
+
+  let xsMinutes = xsMinuteTime;
+  xsMinutes = xsMinuteTime < 10 ? "0" + xsMinuteTime : xsMinuteTime;
+
+  let xsSeconds = xsTime;
+  xsSeconds = xsSeconds < 10 ? "0" + xsSeconds : xsSeconds;
+  extraStudyTimer.innerHTML = `Extra Time: +${xsMinutes}:${xsSeconds}`;
+  xsTime++;
+}
+
+function updateBreakTimer() {
+  let minutes = Math.floor(breakTime / 60);
+  minutes = minutes >= 10 ? minutes : "0" + minutes;
+
+  let seconds = breakTime % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  timer.innerHTML = `${minutes}:${seconds}`;
+  breakTime--;
+
+  if (minutes === "00" && seconds === "00") {
+    clearInterval(breakTimerActive);
+    clearInterval(timerActive);
+    breakIsRunning = false;
+    timer.style.color = "azure";
+    extraStudyTimer.innerHTML = "Back To Work~";
+    timer.innerHTML = `${
+      startingMinutes < 10 ? "0" + startingMinutes : startingMinutes
+    }:00`;
+    play.style.display = "block";
+  }
+}
+
+// Record Daily Total Progress
+let totalHour = 0;
+let totalMinute = 0;
+let totalSecond = 0;
+
+if (totalMinute >= 60) {
+  totalHour++;
+  totalMinute = totalMinute % 60;
+}
+
+if (totalSecond === 60) {
+  totalMinute++;
+  totalSecond = totalSecond % 60;
+}
+
 // Buttons
-document.querySelector(".resume").addEventListener("click", function () {
-  if (!isRunning) {
-    timerActive = setInterval(updateTimer, 1000);
-    isRunning = true;
+window.addEventListener("keydown", function (e) {
+  if (e.code == "Space") {
+    timingConditions();
   }
 });
 
-document.querySelector(".pause").addEventListener("click", function () {
-  clearInterval(timerActive);
-  isRunning = false;
-});
+play.addEventListener("click", timingConditions);
 
-document.querySelector(".restart").addEventListener("click", function () {
+restart.addEventListener("click", function () {
   time = startingMinutes * 60;
   clearInterval(timerActive);
 
@@ -47,3 +126,51 @@ document.querySelector(".restart").addEventListener("click", function () {
     updateTimer();
   }
 });
+
+// Function Code For Timing Conditions
+function timingConditions() {
+  if (breakIsRunningToggle === 2) {
+    clearInterval(timerActive);
+    timerActive = setInterval(updateTimer, 1);
+  }
+
+  if (isRunning) {
+    clearInterval(timerActive);
+    isRunning = false;
+    play.innerHTML = "Play";
+  } else if (!isRunning && !xsIsRunning && !breakIsRunning) {
+    timerActive = setInterval(updateTimer, 1);
+    isRunning = true;
+    play.innerHTML = "Pause";
+  }
+
+  if (xsIsRunning) {
+    clearInterval(xsTimerActive);
+    totalMinute += startingMinutes;
+    xsIsRunning = false;
+    totalMinute += xsMinuteTime;
+    xsMinuteTime = 0;
+    totalSecond += xsTime;
+    xsTime = 0;
+
+    extraStudyTimer.innerHTML = "Break-Time! :)";
+    extraStudyTimer.style.color = "azure";
+    totalTime.innerHTML = `Daily Total Time:   ${totalHour} Hr, ${totalMinute} Min, ${totalSecond} Sec`;
+    play.innerHTML = "Play";
+    timer.style.color = "rgb(125, 255, 125)";
+    breakMinutes < 10
+      ? (breakMinutes = "0" + breakMinutes)
+      : (breakMinutes = breakMinutes);
+    timer.innerHTML = `${breakMinutes}:00`;
+    breakIsRunning = true;
+    // breakTimeIsRunning = setInterval(updateBreakTimer(), 1000);
+  }
+
+  if (breakIsRunning) {
+    if (breakIsRunningToggle === 1) {
+      breakTimerActive = setInterval(updateBreakTimer, 1);
+      play.style.display = "none";
+    }
+    breakIsRunningToggle = breakIsRunningToggle === 0 ? 1 : 2;
+  }
+}
